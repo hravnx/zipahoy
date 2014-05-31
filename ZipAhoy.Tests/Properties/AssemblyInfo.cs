a7f9c89f4d9b4a9e62c90552ca80b1ab7c637a5d
Henrik Ravn <hravnx@gmail.com>
2014-05-31 10:33:49 +0200

    Added empty test project and AssemblyInfo metadate

--- ZipAhoy.Tests/Properties/AssemblyInfo.cs
+++ ZipAhoy.Tests/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZipAhoy")]
-[assembly: AssemblyDescription("Small task-based C# convenience library to compress and extract folders to and from zip files")]
+[assembly: AssemblyTitle("ZipAhoy.Tests")]
+[assembly: AssemblyDescription("Tests for the Zip Ahoy project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ZipAhoy")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4dc55ab7-f0f0-41ea-98d2-a7b105ac97a7")]
+[assembly: Guid("b3c0c6d9-ef69-4244-90ba-ad70dc1b6085")]
 
 // Version information for an assembly consists of the following four values:
 //
