235952c762d7cd972b9970b2488054af8190c426
Henrik Ravn <hravnx@gmail.com>
2014-05-31 11:24:50 +0200

    Added tests for bad create parameters

--- ZipAhoy.Tests/CreateFromFolderTests.cs
+++ ZipAhoy.Tests/CreateFromFolderTests.cs
@@ -11,6 +11,26 @@ namespace ZipAhoy.Tests
     public class CreateFromFolderTests
     {
         [Fact]
+        public void Create_with_bad_arguments_throws()
+        {
+            foreach(var arg in new [] {null, "", "   \t   "})
+            {
+                var ex = Assert.Throws<ArgumentNullException>(() => Archive.CreateFromFolder(arg, "well.zip"));
+                Assert.Equal("folderPath", ex.ParamName);
+
+                ex = Assert.Throws<ArgumentNullException>(() => Archive.CreateFromFolder("somestuff", arg));
+                Assert.Equal("archiveFilePath", ex.ParamName);
+            }
+        }
+        
+        [Fact]
+        public void Create_from_nonexisting_folder_throws()
+        {
+            var name = FileUtils.GetTempFilename(".tmp");
+            Assert.Throws<ArgumentException>(() => Archive.CreateFromFolder(name, "well.zip"));
+        }
+
+        [Fact]
         public async Task Create_from_empty_folder_results_in_empty_zip_file()
         {
             using(var tempFolder = "zip-".CreateTempFolder())
@@ -21,6 +41,8 @@ namespace ZipAhoy.Tests
                 var info = new FileInfo(zipPath);
                 Assert.True(info.Exists);
                 Assert.Equal(0, info.Length);
+
+                info.Delete();
             }
         }
         
--- ZipAhoy/Archive.cs
+++ ZipAhoy/Archive.cs
@@ -12,6 +12,20 @@ namespace ZipAhoy
 
         public static Task<bool> CreateFromFolder(string folderPath, string archiveFilePath)
         {
+            if (String.IsNullOrWhiteSpace(folderPath))
+            {
+                throw new ArgumentNullException("folderPath");
+            }
+            if (String.IsNullOrWhiteSpace(archiveFilePath))
+            {
+                throw new ArgumentNullException("archiveFilePath");
+            }
+
+            if(!Directory.Exists(folderPath))
+            {
+                throw new ArgumentException(String.Format("'{0}' does not exist", folderPath), "folderPath");
+            }
+
             using(var s = File.Create(archiveFilePath))
             {
 
